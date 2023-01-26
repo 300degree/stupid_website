c8c13ba382487e6659853500208ab994433d00cf
KiTiboD Phaphanwoe <101421091+300degree@users.noreply.github.com>
2023-01-27 00:59:53 +0700

    modified:   pages/_document.tsx 	modified:   pages/index.tsx 	components/ 	pages/challenge/

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Navbar from '@/components/nav'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Navbar />
       <body>
         <Main />
         <NextScript />
