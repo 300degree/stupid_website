db2e159ded580e2f08762e51e3d76bde1ccd0208
KiTiboD Phaphanwoe <101421091+300degree@users.noreply.github.com>
2023-01-27 20:40:06 +0700

    modified:   components/nav.tsx 	modified:   pages/challenge/index.tsx 	modified:   pages/index.tsx 	modified:   styles/globals.css 	styles/Nav.module.css

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,15 +1,19 @@
+import styles from '@/styles/globals.css'
 import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div>
-      <h2 className="logo">logo</h2>
-      <nav className="navigation">
-        <Link href="/">Home</Link>
-        <Link href="/challenge">Challenge</Link>
-        <Link href="/">About</Link>
-      </nav>
-    </div>
+    <>
+      <header>
+        <h2 className="logo">Logo</h2>
+        <nav className="navigation">
+          <Link href={"/"}>Home</Link>
+          <Link href={"/Character"}>Character</Link>
+          <Link href={"/Challenge"}>Challenge</Link>
+	        <button className="btnLogin-popup">Login</button>
+        </nav>
+      </header>
+    </>
   )
 }
 
--- pages/challenge/index.tsx
+++ pages/challenge/index.tsx
@@ -3,6 +3,7 @@ import Navbar from '@/components/nav'
 const FlashCardPages = () => {
   return (
     <div>
+      <p>Hello world</p>
     </div>
   )
 }
